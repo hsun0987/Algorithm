d8353e8abe88cc55e29622fb021746044d569bab
MinSun <116503534+hsun0987@users.noreply.github.com>
2024-05-01 10:15:12 +0900

    [Bronze V] Title: 아스키 코드, Time: 200 ms, Memory: 17560 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+       Scanner in = new Scanner(System.in);
+       String a = in.next();
+       char answer = a.charAt(0);
+
+        System.out.println((int) answer);
+    }
+}
