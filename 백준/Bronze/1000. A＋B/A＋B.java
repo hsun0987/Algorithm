622329faebe96f64d2627cf49cb0eaf297636520
MinSun <116503534+hsun0987@users.noreply.github.com>
2024-05-04 23:31:33 +0900

    [Bronze V] Title: A+B, Time: 208 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static int solution(int a, int b) {
+        int answer;
+        answer = a + b;
+        return answer;
+    }
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+        int b = scanner.nextInt();
+
+        int answer = solution(a, b);
+        System.out.println(answer);
+    }
+}
