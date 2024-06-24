2e7f35ea4486203691855bd374ad60b3b8c6ccea
MinSun <116503534+hsun0987@users.noreply.github.com>
2024-06-24 17:17:04 +0900

    [Silver IV] Title: 2+1 세일, Time: 3852 ms, Memory: 35364 KB -BaekjoonHub

---
+++ 백준/Silver/11508. 2＋1 세일/2＋1 세일.py
@@ -0,0 +1,12 @@
+# 2+1 가장 싼 것 무료 3으로 나눠떨어질 때 합X
+
+n = int(input())
+arr = [int(input()) for i in range(n)]
+
+arr.sort(reverse=True)
+answer = 0
+for i in range(n):
+    if (i+1) % 3 != 0:
+        answer += arr[i]
+
+print(answer)
