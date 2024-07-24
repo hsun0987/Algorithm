db99d603e3c1a21ec7d7e84cdbc82892eafaf83e
MinSun <116503534+hsun0987@users.noreply.github.com>
2024-07-24 12:42:25 +0900

    [Silver II] Title: 가장 큰 증가하는 부분 수열, Time: 164 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
+++ 백준/Silver/11055. 가장 큰 증가하는 부분 수열/가장 큰 증가하는 부분 수열.py
@@ -16,7 +16,7 @@ for i in range(1, n):
    for j in range(i):
       if arr[i] > arr[j]:
          dp[i] = max(dp[i], dp[j] + arr[i])
-      if dp[i] == 0:
-         dp[i] = arr[i]
+   if dp[i] == 0: # 반례 2 1 2 => output: 2, answer: 3
+      dp[i] = arr[i]
 
 print(max(dp))
