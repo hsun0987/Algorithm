47a2ecff003007a36aca5a1c0627d3bfeaec03ee
MinSun <116503534+hsun0987@users.noreply.github.com>
2024-07-25 12:44:14 +0900

    [Silver III] Title: 조합, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2407. 조합/조합.py
@@ -0,0 +1,22 @@
+# 연결된 숫자 중 가장 긴 부분 수열
+import sys
+
+input = sys.stdin.readline
+n, r = map(int, input().split())
+
+# 초기값 설정
+dp = [[0] * 101 for _ in range(101)]
+
+# 점화식
+# nCr = n-1Cr-1 + n-1Cr
+# dp[r][n] = dp[r-1][n-1] +dp[r][n-1]
+for i in range(r+1):
+   for j in range(n+1):
+      if i == 1:
+         dp[i][j] = j
+      elif i == j:
+         dp[i][j] = 1
+      elif i < j:
+         dp[i][j] = dp[i-1][j-1] +dp[i][j-1]
+
+print(dp[r][n])
