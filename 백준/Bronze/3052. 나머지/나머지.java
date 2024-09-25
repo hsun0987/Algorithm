026c77c349564990b5195f997af4c715aa0a3ef7
MinSun <116503534+hsun0987@users.noreply.github.com>
2024-09-25 14:41:57 +0900

    [Bronze II] Title: 나머지, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.java
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -5,8 +5,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        // 중복한 값은 저장X => 서로 다른 나머지 값만 저장 
-        HashSet set = new HashSet();
+        // 중복한 값은 저장X => 서로 다른 나머지 값만 저장
+        HashSet<Integer> set = new HashSet<>();
 
         for(int i = 0; i < 10; i++){
             int n = Integer.parseInt(br.readLine());
