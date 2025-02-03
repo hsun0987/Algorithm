fbb552353ca2ac6b2db59a3645914e2b8566a72a
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-02-03 15:11:45 +0900

    [level 2] Title: 가장 큰 수, Time: 2911.01 ms, Memory: 399 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+class Solution {
+    public String solution(int[] numbers) {
+        String answer = "";
+        
+        // 문자열로 바꿔서 저장
+        String[] str = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            str[i] = String.valueOf(numbers[i]);
+        }
+
+        // 내림차순 정렬
+        // o1 : 10, o2 : 2 이면 102과 210 중 뭐가 더 큰지 비교
+        Arrays.sort(str, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        // 첫번째 값 = 0, 0으로만 이루어진 배열 -> 0 리턴
+        if (str[0].equals("0")){
+            return "0";
+        }
+
+        for (String s : str){
+            answer += s;
+        }
+        
+        return answer;
+    }
+}
