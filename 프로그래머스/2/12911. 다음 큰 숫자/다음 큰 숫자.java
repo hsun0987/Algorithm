662a80554097de6935f77bfb6c58636d25da990d
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-02-11 19:00:30 +0900

    [level 2] Title: 다음 큰 숫자, Time: 0.04 ms, Memory: 53.5 MB -BaekjoonHub

--- 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
+++ 프로그래머스/2/12911. 다음 큰 숫자/다음 큰 숫자.java
@@ -3,27 +3,15 @@ class Solution {
         int answer = 0;
 
         // 1. n을 2진수로 변환 후 1의 갯수 새기
-        String binaryN = Integer.toBinaryString(n);
-        int bN = 0;
-        for (int i = 0; i < binaryN.length(); i++) {
-            if (binaryN.charAt(i) == '1'){
-                bN++;
-            }
-        }
-        
+        int bN = Integer.bitCount(n);
+
         while(true){
             // 2. 1씩 증가
             n += 1;
 
             // 3. 2진수 변환 수 1의 개수 세기
-            String b = Integer.toBinaryString(n);
-            int cnt = 0;
-            for (int i = 0; i < b.length(); i++) {
-                if (b.charAt(i) == '1'){
-                    cnt++;
-                }
-            }
-            
+            int cnt = Integer.bitCount(n);
+
             // 3. n의 1 개수와 같으면 break
             if (bN == cnt){
                 answer = n;
