1f3adc0d687403543a0250c5a6698bd361e245e4
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-02-22 15:27:06 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 85.9 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
 
 ### 성능 요약
 
-메모리: 82 MB, 시간: 0.06 ms
+메모리: 85.9 MB, 시간: 0.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 22일 15:03:56
+2025년 02월 22일 15:27:04
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,38 +1,23 @@
 class Solution {
-    static int n, m;
-    
     public int[] solution(int brown, int yellow) {
         int[] answer = new int[2];
-        
-         // 1. 노랑의 격자배열을 1줄, 2줄 3줄 .. 차례로 탐색
-        // 2. 필요한 갈색 개수가 같다면 정답
-        // - 노랑 격자(n*m) --> (n+m)*2+4 = 갈색 개수
-        // 3. 전체 크기 = n+2, m+2
 
-        n = 1;
-        m = yellow / n;
+        // 1. 전체 크기 = brown + yellow 의 약수
+        // 2. yellow 개수와 적합한 크기 찾기
+        // - 가로 = 합 / i (i = 세로), yellow = (가로-2) * (세로-2)
 
-        while (!isBrown(brown, yellow)){
-            isBrown(brown, yellow);
-        }
-
-        answer[0] = Math.max(n+2, m+2);
-        answer[1] = Math.min(n+2, m+2);
-
-        return answer;
-    }
-
-    public static boolean isBrown(int b, int y) {
-        if (((n + m) * 2 + 4) != b){
-            n += 1;
-            // 직사각형 -> 약수로 격자 만듦
-            while (y % n != 0){
-                n += 1;
+        // 최소 크기 = 3 (y=1 일 때)
+        for (int i = 3; i < brown+yellow ; i++) {
+            int width = (brown + yellow) / i;
+            if (width >= i){ // 가로가 더 길다
+                if (yellow == (width-2) * (i-2)){
+                    answer[0] = width;
+                    answer[1] = i;
+                    break;
+                }
             }
-            m = y / n;
-            return false;
-        }else{
-            return true;
         }
+        
+        return answer;
     }
 }
