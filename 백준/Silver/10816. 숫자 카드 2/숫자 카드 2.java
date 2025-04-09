2f32beb05aa5a5802a9777589d20b9ac43941338
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-09 14:28:41 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1048 ms, Memory: 192112 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -6,32 +6,33 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 1. 가지고 있는 카드 배열1
-        // - 해시맵 (카드번호, 카드개수)
-        // 2. 찾을 숫자 배열2
-        // 3. 배열1에 배열2가 몇개 포함되어 있는지
-        // - 카드번호와 같은 키 찾고 그 값 리턴
+        // 1. map에 가지고 있는 카드와 갯수 저장
+        // 2. (숫자카드, 갯수)
+        // 3. 찾을 카드를 map에서 찾기 key가 없으면 0
 
         HashMap<Integer, Integer> map = new HashMap<>();
-
         int n = Integer.parseInt(br.readLine());
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            map.put(num, map.getOrDefault(num, 0) + 1);
+            int key = Integer.parseInt(st.nextToken());
+            map.put(key, map.getOrDefault(key, 0) + 1);
         }
 
         int m = Integer.parseInt(br.readLine());
-        st = new StringTokenizer(br.readLine(), " ");
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
         for (int i = 0; i < m; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            if (map.containsKey(num)){
-                bw.write(map.get(num) + " ");
+            int key = Integer.parseInt(st.nextToken());
+            if (map.containsKey(key)){
+                sb.append(map.get(key) + " ");
             }else{
-                bw.write("0 ");
+                sb.append("0 ");
             }
         }
 
+        bw.write(sb + "");
         bw.flush();
         bw.close();
     }
