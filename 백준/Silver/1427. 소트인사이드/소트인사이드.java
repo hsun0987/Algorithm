e4c224eb33e0c7cbd0082a619316ad08ccc9b2c7
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-09 14:11:17 +0900

    [Silver V] Title: 소트인사이드, Time: 100 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Silver/1427. 소트인사이드/소트인사이드.java
+++ 백준/Silver/1427. 소트인사이드/소트인사이드.java
@@ -6,15 +6,13 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 1. 입력값을 잘라서 배열로 저장
-        String[] arr = br.readLine().split("");
+        // 1. 입력값을 잘라서 배열로 저장(String -> char arr)
+        char[] arr = br.readLine().toCharArray();
         // 2. 정렬
         Arrays.sort(arr);
 
-        String answer = "";
-        for (int i = arr.length-1; i >= 0; i--) {
-            answer += arr[i];
-        }
+        // 문자열로 변경 후 뒤집기(StringBuilder 사용)
+        String answer = new StringBuilder(new String(arr)).reverse().toString();
 
         bw.write(answer);
         bw.flush();
