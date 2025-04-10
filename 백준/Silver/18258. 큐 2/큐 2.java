dd534929c4846fda93fd5c77720b3d39a4fd0a31
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-10 11:11:06 +0900

    [Silver IV] Title: 큐 2, Time: 1280 ms, Memory: 350032 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -2,51 +2,54 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        LinkedList<Integer> q = new LinkedList<>();
 
-        int t = Integer.parseInt(br.readLine());
-        for (int i = 0; i < t; i++) {
-            String[] input = br.readLine().split(" ");
-            switch (input[0]){
+        Deque<Integer> q = new LinkedList<>();
+        int n = Integer.parseInt(br.readLine());
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String op = st.nextToken();
+
+            switch (op){
                 case "push":
-                    q.add(Integer.parseInt(input[1]));
+                    int x = Integer.parseInt(st.nextToken());
+                    q.addLast(x);
                     break;
                 case "pop":
-                    if(q.isEmpty()){
-                        bw.write("-1 \n");
-                        break;
+                    if (!q.isEmpty()){
+                        bw.write(q.poll() + "\n");
+                    }else{
+                        bw.write("-1\n");
                     }
-                    bw.write(q.poll() +"\n");
                     break;
                 case "size":
                     bw.write(q.size() + "\n");
                     break;
                 case "empty":
-                    if(q.isEmpty()){
-                        bw.write("1 \n");
-                    }else
-                        bw.write("0 \n");
+                    if (q.isEmpty()){
+                        bw.write("1\n");
+                    }else{
+                        bw.write("0\n");
+                    }
                     break;
                 case "front":
-                    if(q.isEmpty()){
-                        bw.write("-1 \n");
-                        break;
+                    if (!q.isEmpty()){
+                        bw.write(q.getFirst() + "\n");
+                    }else{
+                        bw.write("-1\n");
                     }
-                    bw.write(q.getFirst() + "\n");
                     break;
                 case "back":
-                    if(q.isEmpty()){
-                        bw.write("-1 \n");
-                        break;
+                    if (!q.isEmpty()){
+                        bw.write(q.getLast() + "\n");
+                    }else{
+                        bw.write("-1\n");
                     }
-                    bw.write(q.getLast() + "\n");
                     break;
             }
         }
-
         bw.flush();
         bw.close();
     }
