0df4ee2da0376df1b6b6309f9eeddeb589e3da54
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-11 17:30:22 +0900

    [Silver IV] Title: 로프, Time: 268 ms, Memory: 26540 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -11,19 +11,18 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
-        br.close();
 
         Arrays.sort(arr);
-        
-        int answer = 0;
 
+        int answer = 0;
         for (int i = 0; i < arr.length; i++) {
+            // 최솟값을 기준으로 갯수에 따라 들 수 있는 무게를 구함
+            // w/k -> w = 무게 * k
             int x = arr[i] * (n-i);
             if(x > answer){
                 answer = x;
             }
         }
-
         bw.write(answer + "");
         bw.flush();
         bw.close();
