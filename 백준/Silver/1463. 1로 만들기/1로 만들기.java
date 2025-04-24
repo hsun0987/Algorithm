d54f85f78c0de8d604bc3c3b22673312f3850d7f
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-24 15:07:33 +0900

    [Silver III] Title: 1로 만들기, Time: 136 ms, Memory: 18304 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,4 +1,5 @@
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -7,26 +8,22 @@ public class Main {
 
         int n = Integer.parseInt(br.readLine());
         int[] dp = new int[n+1];
-
-        // n=1
         dp[1] = 0;
 
-        for(int i = 2; i <= n; i++) {
-            // +1, %2, %3 연산 모두 한 후 최소값 저장
-            // dp[2]부터 이전값+1, i//2 + 1, i//3 + 1 값 중 최솟값을 저장함
-            dp[i] = dp[i-1]+1;
-            if(i % 2 == 0) {
-            	if(dp[i/2]+1 < dp[i]) {
-            		dp[i] = dp[i/2]+1;
-            	}
+        //2 //3 -1 이 될 수 있는 경우를 구한 후 그 중 최솟값을 dp에 저장
+        // dp[n] = min(dp[n-1] + 1, dp[n//2], dp[n//3])
+        // 이전값+1, 2의 배수일 경우+1, 3의 배수일 경우+1
+
+        for (int i = 2; i < n+1; i++) {
+            dp[i] = dp[i-1] + 1;
+
+            if (i % 2 == 0){
+                dp[i] = Math.min(dp[i], dp[i/2] + 1);
             }
-            if(i % 3 == 0) {
-            	if(dp[i/3]+1 < dp[i]) {
-            		dp[i] = dp[i/3]+1;
-            	}
+            if (i % 3 == 0){
+                dp[i] = Math.min(dp[i], dp[i/3] + 1);
             }
         }
-        
         bw.write(dp[n] + "");
         bw.flush();
         bw.close();
