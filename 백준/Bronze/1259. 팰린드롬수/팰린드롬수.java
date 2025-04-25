6bf4f816d522abadf315bc16d5c0e99612d652e8
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-25 14:58:31 +0900

    [Bronze I] Title: 팰린드롬수, Time: 92 ms, Memory: 14028 KB -BaekjoonHub

--- 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -2,29 +2,25 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-        StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        while(true) {
-            String s = sc.next();
-            boolean flag = true;
-
-            if(s.equals("0")){
+        while(true){
+            String str = br.readLine();
+            StringBuilder sb = new StringBuilder(str);
+            if (str.equals("0")){
                 break;
             }
-
-            for (int i = 0; i < s.length()/2; i++) {
-                if(s.charAt(i) != s.charAt(s.length()-i -1)) {
-                    sb.append("no").append("\n");
-                    flag = false;
-                    break;
-                }
-            }
-            if(flag) {
-                sb.append("yes").append("\n");
+            sb.reverse();
+            if (str.equals(sb + "")){
+                bw.write("yes \n");
+            }else{
+                bw.write("no \n");
             }
         }
-        System.out.println(sb);
+
+        bw.flush();
+        bw.close();
     }
 }
