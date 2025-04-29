034fcafa41d84394425b421b786cf9ab5f8e5704
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-04-29 18:56:26 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 144 ms, Memory: 17244 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
@@ -5,13 +5,13 @@ public class Main {
     static String[] arr;
     static String[] p;
     static int max;
-    static int num;
+    static int n;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        String n = st.nextToken();
+        n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
 
         st = new StringTokenizer(br.readLine());
@@ -21,44 +21,44 @@ public class Main {
         }
 
         Arrays.sort(arr);
-        p = new String[n.length()];
+        // int의 자릿수를 구함
+        int len = (int)Math.log10(n) + 1;
+        p = new String[len];
         max = 0;
-        num = Integer.parseInt(n);
+
         // 조합 구하기
         // 중복순열 : 순서를 고려하면서 서로 다른 n개에서 중복으로 r개를 일렬로 나열하는 수
         // 3자리수면 -> 1, 2, 3 자리수일 때 다 구해주기
         // 반례 : 100 3 -> 1 2 3 max = 33
-        for (int i = n.length(); i > 0; i--) {
+        for (int i = len; i > 0; i--) {
             permutation(0, k, i);
         }
-
 
         bw.write(max + "");
         bw.flush();
         bw.close();
     }
 
-    static void permutation(int depth, int n, int r){
+    static void permutation(int depth, int N, int R){
         // r개를 다 고른 경우
-        if (depth == r){
+        if (depth == R){
             // 배열에 담긴 순열을 문자열로 바꾸기
-            String str = "";
-            for (int i = 0; i < r; i++) {
-                str += p[i];
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < R; i++) {
+                sb.append(p[i]);
             }
             // N보다 작은 값 중 최댓값 누적
-            int now  = Integer.parseInt(str);
-            if (num >= now){
+            int now  = Integer.parseInt(sb.toString());
+            if (n >= now){
                 max = Math.max(max, now);
             }
             return;
         }
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < N; i++) {
             // 1개씩 이어 붙인다
             p[depth] = arr[i];
-            permutation(depth+1, n, r); // depth + 1 전달
+            permutation(depth+1, N, R); // depth + 1 전달
         }
-
     }
 }
