7cb960233a99b989766ba5ca75d2c13400eda2fa
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-05-17 11:24:28 +0900

    [Silver II] Title: 나무 자르기, Time: 512 ms, Memory: 119612 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -2,8 +2,6 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    static int[] ans = new int[2];
-    static int[][] arr;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
@@ -14,33 +12,36 @@ public class Main {
 
         int[] arr = new int[n];
         st = new StringTokenizer(br.readLine());
+
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        // 범위 초과 이슈로 타입 int -> long 변환
+        // start = 최소길이 end = 최대길이
+        // 잘라진 길이의 합이 최대
+        // sum(나무 길이 - mid) >= M -> 오른쪽 탐색
+
         long start = 1;
         long end = Arrays.stream(arr).max().getAsInt();
 
-        while (start <= end){
-            // 중간 값을 기준으로 탐색
+        while(start <= end){
             long mid = (start + end) / 2;
-            long len = 0;
 
+            long sum = 0;
             for(int a : arr){
-                if (a > mid)
-                    len += a - mid;
+                if(a > mid){
+                    sum += a - mid;
+                }
             }
 
-            // 원하는 길이보다 짧으면 더 분리
-            if (len < m){
+            if (sum < m){
                 end = mid - 1;
             }else{
                 start = mid + 1;
             }
         }
 
-        bw.write(end + "\n");
+        bw.write(end + "");
         bw.flush();
         bw.close();
     }
