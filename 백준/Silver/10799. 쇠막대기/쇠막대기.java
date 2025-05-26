617e5d68539b0eb00c078906040e8a0bd461b25c
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-05-26 11:17:57 +0900

    [Silver II] Title: 쇠막대기, Time: 152 ms, Memory: 16552 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -6,24 +6,28 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        // 1. 레이저를 쏘면 스택의 사이즈 만큼 더해준다 = 레이저를 쏘고 나눠진 조각 수
-        // 2. 막대가 끝나면 +1 = 레이저를 쏜 다음 남은 1조각
+        char[] input = br.readLine().toCharArray();
         Stack<Character> stack = new Stack<>();
         int answer = 0;
 
-        char[] input = br.readLine().toCharArray();
         for (int i = 0; i < input.length; i++) {
-            if(input[i] == '('){
-                stack.push(input[i]);
-            }else{
-                stack.pop();
-                if(input[i-1] == '('){
-                    answer += stack.size();
-                }else{
-                    answer += 1;
-                }
+            switch (input[i]){
+                case '(':
+                    stack.push('(');
+                    break;
+                case ')':
+                    stack.pop();
+                    if(input[i-1] == '('){
+                        // 레이저
+                        answer += stack.size();
+                    }else{
+                        // 막대기 끝
+                        answer += 1;
+                    }
+                    break;
             }
         }
+
         bw.write(answer + "");
         bw.flush();
         bw.close();
