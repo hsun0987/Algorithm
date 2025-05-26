3a63a18a6922acf32452ca5a5f4b14c9b92df35f
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-05-26 12:29:39 +0900

    [Silver V] Title: 막대기, Time: 108 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.java
+++ 백준/Silver/1094. 막대기/막대기.java
@@ -1,36 +1,14 @@
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        Stack<Integer> list = new Stack<>();
         int X = Integer.parseInt(br.readLine());
+        // 비트마스크 이용 -> 2진수 변환했을 때 1의 개수
+        int answer = Integer.bitCount(X);
 
-        list.add(64);
-        int sum = 64;
-
-        while(true){
-            if (sum == X){
-                break;
-            }
-            sum = 0;
-            // 1. 절반을 자르고 넣어
-            int half = list.pop() / 2;
-            list.add(half);
-            // 2. 합을 구해
-            for(int i : list){
-                sum += i;
-            }
-            // 3. 합이 X보다 큰지 비교(작으면 절반 자른거 안버려)
-            if(sum < X){
-                list.add(half);
-            }
-        }
-
-        int answer = list.size();
         bw.write(answer + "");
         bw.flush();
         bw.close();
