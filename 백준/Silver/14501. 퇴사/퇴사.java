32a624dc3db8c1a4a6e3c1114156e8819c1b97e3
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-06-03 14:21:16 +0900

    [Silver III] Title: 퇴사, Time: 104 ms, Memory: 14272 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -16,15 +16,15 @@ public class Main {
             TP[i] = new int[]{T, P};
         }
 
-        int[] dp = new int[N+2]; // N+1 까지 고려
+        int[] dp = new int[N+1];
 
-        for (int i = 0; i < N+1; i++) {
+        for (int i = 0; i < N; i++) {
             int t = TP[i][0];
             int p = TP[i][1];
 
             // i일에 상담 하지 않은 경우(0 방지)
             dp[i+1] = Math.max(dp[i+1], dp[i]);
-
+            
             if(i+t <= N){
                 dp[i+t] = Math.max(dp[i+t], p + dp[i]);
             }
