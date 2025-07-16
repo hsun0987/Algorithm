589d6b983e668b92fd043fba24634641ed58c3a0
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-07-16 19:45:23 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 95.52 ms, Memory: 97 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -4,26 +4,26 @@ class Solution {
     public String solution(String[] participant, String[] completion) {
         String answer = "";
         
-        // 완주자 명단에 없는 사람의 이름 반환 -> 동명이인 고려
+        // 1. 명단 hash map 생성 -> 동명이인을 고려하여 value 에 인원수 저장
+        // 2. completion 에 이름이 있을 경우 value -1
+        // 3. value != 0 일 경우 완주하지 못한 사람
         
-        // 1. key: 이름 value: 동명이인 개수
         HashMap<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < participant.length; i++) {
-            map.put(participant[i], map.getOrDefault(participant[i], 0) + 1);
+        for(String name : participant){
+            map.put(name, map.getOrDefault(name, 0) + 1);
         }
         
-        // 2. 완주자에 key가 있으면 value -1
-        for (String name : completion){
-            map.put(name, map.get(name) -1);
-        }
-        
-        // 3. value가 1인 key 값 리턴
-        for(String key: map.keySet()){
-            if (map.get(key) == 1){
-                answer = key;
+        for(String name : completion){
+            map.put(name, map.get(name) - 1);
+            
+            if(map.get(name) == 0){
+                map.remove(name);
             }
         }
         
+        for(String name : map.keySet()){
+            answer = name;
+        }
         return answer;
     }
 }
