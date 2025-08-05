07e705ac924bb7c575918c9f37bfb0551ed3a8c3
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-08-05 14:09:20 +0900

    [Silver III] Title: 두 수의 합, Time: 296 ms, Memory: 25448 KB -BaekjoonHub

---
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(arr);
+
+        int X = Integer.parseInt(br.readLine());
+        int answer = 0;
+        int start = 0, end = N-1;
+
+        while (start < end){
+
+            int sum = arr[start] + arr[end];
+            if (sum == X){ // 범위 좁히기
+                answer++;
+                start++;
+                end--;
+            }else if(sum < X){
+                start++;
+            }else{
+                end--;
+            }
+        }
+
+
+        bw.write(answer + "");
+        bw.flush();
+        bw.close();
+    }
+}
