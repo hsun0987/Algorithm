6cf13eeb3eb75146cc46698871cd6dcba3c84c4d
MinSun <116503534+hsun0987@users.noreply.github.com>
2025-08-05 15:40:55 +0900

    [Silver I] Title: 절댓값 힙, Time: 320 ms, Memory: 26208 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -5,42 +5,36 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        PriorityQueue<Integer> pq = new PriorityQueue<>(new PriorityComparator());
 
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            int x = Integer.parseInt(br.readLine());
-            if(x == 0){
-                if(pq.isEmpty()){
-                    bw.write("0 \n");
+        PriorityQueue<Integer> pq = new PriorityQueue<>(new Comparator<Integer>() {
+            @Override
+            public int compare(Integer o1, Integer o2) {
+                int abs1 = Math.abs(o1);
+                int abs2 = Math.abs(o2);
+
+                if (abs1 == abs2){ // 더 작은 값이 먼저
+                    return o1 - o2;
                 }else{
-                    bw.write(pq.poll() + "\n");
+                    return abs1 - abs2;
                 }
+            }
+        });
+
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int input = Integer.parseInt(br.readLine());
+            if (input == 0){
+                if (pq.isEmpty()){
+                    bw.write("0 \n");
+                    continue;
+                }
+                bw.write(pq.poll() + "\n");
             }else{
-                pq.add(x);
+                pq.add(input);
             }
         }
+
         bw.flush();
         bw.close();
-    }
-
-    static class PriorityComparator implements Comparator<Integer>{
-        @Override
-        public int compare(Integer o1, Integer o2) {
-            // 1 : 뒤로(더 큰 값), -1 : 앞으로(더 작은 값), 0 : 그대로(같은 값)
-            if (Math.abs(o1) > Math.abs(o2)) {
-                return 1;
-            } else if(Math.abs(o1) < Math.abs(o2)){
-                return -1;
-            } else{
-                if(o1 > o2){
-                    return 1;
-                }else if(o1 < o2){
-                    return -1;
-                }else {
-                    return 0;
-                }
-            }
-        }
     }
 }
