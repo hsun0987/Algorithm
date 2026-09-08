868fa08c8a61a9b1b0c862a7898b08aa5d9838d2
MinSun <116503534+hsun0987@users.noreply.github.com>
2026-09-08 09:24:46 +0900

    [level 0] Title: 양꼬치, Time: 0.02 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120830. 양꼬치/양꼬치.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int n, int k) {
+        int answer = 0;
+        
+        answer = n * 12000;
+        
+        if (n/10 > 0) {
+            k = (k - (n/10));
+        } 
+        
+        answer += k * 2000;
+        
+        return answer;
+    }
+}
