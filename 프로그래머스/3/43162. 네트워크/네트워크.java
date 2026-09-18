93920431ac8894a4070029a92f6d0d145b6ab713
MinSun <116503534+hsun0987@users.noreply.github.com>
2026-09-18 11:19:15 +0900

    [level 3] Title: 네트워크, Time: 0.17 ms, Memory: 81.5 MB -BaekjoonHub

---
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -0,0 +1,33 @@
+class Solution {
+    int n;
+    int[][] computers;
+    boolean[] visited;
+    
+    public int solution(int n, int[][] computers) {
+        this.n = n;
+        this.computers = computers;
+        this.visited = new boolean[n];
+        int answer = 0;
+        
+         for (int i = 0; i < n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+    
+    public void dfs(int node) {
+        visited[node] = true;
+
+        for (int i = 0; i < n; i++) {
+            if (!visited[i] && computers[node][i] == 1) {
+                dfs(i);
+            }
+        }
+
+    }
+    
+}
